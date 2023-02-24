72bcccf90ca18c5e82447b8d6e857c94190a4435
yxxung <loveus011@naver.com>
2023-02-24 15:48:28 +0900

    Day 05

--- 박지영/1차원 배열/10811.js
+++ 박지영/1차원 배열/10811.js
@@ -1,24 +1,41 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 
-let line = input[0].split(' ');
-let N = +line[0];
-let M = +line[1];
+let [N, M] = input[0].split(" ").map(Number);
+const arr = Array.from({ length: N }, (_, i) => i + 1);
 
-let I, J = 0;
-const result = [];
-for (let i = 0; i < N; i++) {
-    result[i] = i + 1;
+for (let i = 1; i < input.length; i++) {
+  let [a, b] = input[i].split(" ").map(Number);
+
+  let tmp = arr.splice(a - 1, b - a + 1).reverse();
+  arr.splice(a - 1, 0, ...tmp);
 }
 
-for (let i = 1; i <= M; i++) {
-    const arr = input[i].split(' ');
-    I = arr[0];
-    J = arr[1];
+console.log(...arr);
 
-    for (let j = J; j >= I; j--) {
-        let temp = result[j];
-        result[j] = result[j - 1];
-        result[j - 1] = temp;
-    }
-}
-result.forEach((i) => process.stdout.write(i + ' '))
+
+// let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+// let line = input[0].split(' ');
+// let N = +line[0];
+// let M = +line[1];
+
+// let I, J = 0;
+// const result = [];
+// for (let i = 0; i < N; i++) {
+//     result[i] = i + 1;
+// }
+
+// for (let i = 1; i <= M; i++) {
+//     const arr = input[i].split(' ');
+//     I = arr[0];
+//     J = arr[1];
+//     console.log('I, J:', I, J)
+
+//     for (let j = I - 1; j < J; j++) {
+//         let temp = result[j];
+//         result[j] = result[j+1];
+//         result[j+1] = temp;
+//     }
+// }
+// 앞으로 공백있는 출력은 이렇게 쓰자
+// console.log(result.join(" "));
