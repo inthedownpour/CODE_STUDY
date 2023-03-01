8b2eb58b67545d6e55ccd0c945330895a75b4c69
yxxung <loveus011@naver.com>
2023-03-01 23:35:36 +0900

    [BOJ - 2743] 단어 길이 재기

--- 박지영/문자열/11720.js
+++ 박지영/문자열/11720.js
@@ -1,6 +1,5 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let N = +input[0];
 let nums = input[1].split('');
 let result = 0;
 
@@ -9,3 +8,6 @@ nums.forEach(e => {
 });
 
 console.log(result);
+
+// console.log((require('fs').readFileSync('/dev/stdin')+'').split('\n')[1]
+// .split('').reduce(function(a,b){return +a+(+b)}))
