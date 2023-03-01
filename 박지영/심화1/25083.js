a70391f620544f0e6c939315f05963c7ccbbede0
yxxung <loveus011@naver.com>
2023-03-01 23:15:23 +0900

    [BOJ - 25083] 새싹

---
+++ 박지영/심화1/25083.js
@@ -0,0 +1,9 @@
+// 역슬래시와 쌍따옴표를 출력할 때는
+// \ 추가해주기
+
+console.log("         ,r'\"7")
+console.log("r`-_   ,'  ,/")
+console.log(" \\. \". L_r'")
+console.log("   `~\\/")
+console.log("      |")
+console.log("      |")
