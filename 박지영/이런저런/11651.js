180d326530d5510e6726d8dfe6c3b3e75641bc36
yxxung <loveus011@naver.com>
2023-03-20 16:07:50 +0900

    [BOJ - 11651] 좌표 정렬하기 2

--- 박지영/이런저런/11651.js
+++ 박지영/이런저런/11651.js
@@ -1,28 +1,24 @@
-// 2차원 배열의 sort() 사용법!
 const input = require('fs').readFileSync(0).toString().trim().split('\n');
 const testCase = input.shift();
 
 let arr = [];
 for (let i = 0; i < testCase; i++) {
     const cells = [];
-    for (let j = 0; j < 2; j++) {
+    for (let j = 1; j >= 0; j--) {
         cells.push(input[i].split(' ')[j])
     }
     arr.push(cells)
 }
-// 시간 초과 조심 ㅜㅜ
-// 1. sort() 1번만 사용하기
-// 2. console.log() 1번만 사용하기
+
 let answer = '';
 arr.sort((a, b) => {
-    // sort()의 2차원 배열 정렬
     if (a[0] === b[0]) {
         return a[1] - b[1];
     } else {
         return a[0] - b[0];
     };
 }).forEach(e => {
-    answer += `${e[0]} ${e[1]}\n`;
+    answer += `${e[1]} ${e[0]}\n`;
 })
 
 console.log(answer);
