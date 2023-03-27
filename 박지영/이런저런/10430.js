4abda3377fd9572f70309a7b5214df1d6c420f89
yxxung <loveus011@naver.com>
2023-03-27 13:19:12 +0900

    [BOJ - 10430] 나머지

---
+++ 박지영/이런저런/10430.js
@@ -0,0 +1,6 @@
+let [a, b, c] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
+
+console.log((a + b) % c);
+console.log(((a % c) + (b % c)) % c);
+console.log((a * b) % c);
+console.log(((a % c) * (b % c)) % c);
