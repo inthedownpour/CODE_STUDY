06951b6fd1b8a365a834359156052eb4f5c4cf19
yxxung <loveus011@naver.com>
2023-03-28 17:06:02 +0900

    [BOJ - 1929] 소수 구하기

--- 박지영/이런저런/1929.js
+++ 박지영/이런저런/1929.js
@@ -1,6 +1,4 @@
-let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
-let n = +input[0];
-let nums = input[1].split(' ').map(Number);
+let [a, b] = require("fs").readFileSync("/dev/stdin").toString().trim().split(' ').map(Number);
 
 //1과 자신만으로 나누어 떨어지는지 체크
 const isPrime = (n) => {
@@ -17,4 +15,8 @@ const isPrime = (n) => {
     return true;
 }
 
-console.log(nums.filter(v => isPrime(v)).length);
+for (let i = a; i <= b; i++) {
+    if (isPrime(i) === true) {
+        console.log(i);
+    }
+}
