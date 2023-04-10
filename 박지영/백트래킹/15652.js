08e76de406cf1978daee935672fb43471b758e4f
yxxung <loveus011@naver.com>
2023-04-10 16:35:23 +0900

    [BOJ - 15652] N과 M (4)

---
+++ 박지영/백트래킹/15652.js
@@ -0,0 +1,24 @@
+const [n, m] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
+
+let result = '';
+let arr = [];
+
+// 비내림차순 체크용 변수 추가
+// 0값인 변수를 넣어서 현재 i와 비교해서 큰지, 작은지를 비교
+const solution = (k, check) => { // 현재 k개까지 수를 택한 상황에서 arr[k] 정하기
+    if (k === m) { // m개를 모두 택했으면
+        result += `${arr.join(' ')}\n`
+        return; //return을 만나면 돌아감
+    }
+
+    for (let i = 1; i <= n; i++) { // 1부터 n까지의 수에 대해
+        if (check <= i) {
+            arr[k] = i; // k번째 수를 i로 정함
+            check = i;
+            solution(k + 1, check); // 다음 수를 정하러 한 단계 더 들어감
+        }
+    }
+}
+
+solution(0, 0);
+console.log(result);
