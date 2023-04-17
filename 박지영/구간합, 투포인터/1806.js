457ba79a520e8421a177b1d4c5a979a92414c8cf
yxxung <loveus011@naver.com>
2023-04-17 22:04:20 +0900

    [BOJ - 1806] 부분합

---
+++ 박지영/구간합, 투포인터/1806.js
@@ -0,0 +1,25 @@
+// 투포인터
+const [[n, s], arr] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map((e) => e.split(' ').map(Number));
+
+let sum = 0;
+let minLength = Infinity;
+let start = 0; // 시작
+let end = 0; // 끝
+
+// 반복문을 통해 j를 증가시키면서
+while (end < n) {
+    // arr[end]를 sum에 누적
+    sum += arr[end];
+
+    // sum이 s 이상일 때, s 미만이 될 때 까지 start를 증가시키면서
+    // sum이 s 이상일 때의 길이를 갱신
+    while (sum >= s) {
+        sum -= arr[start];
+        minLength = Math.min(minLength, end - start + 1);
+        start++;
+    }
+    end++;
+}
+
+// 반복문이 끝난 후에 minLength가 갱신된 적이 없다면 0, 그 외에는 minLength 반환
+console.log(minLength === Infinity ? 0 : minLength);
